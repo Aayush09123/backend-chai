cc76d783c1b9fe1edd3b33d9c31315bb89ab45fd
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-01-16 17:49:26 +0530

    Fix bug in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -425,4 +425,4 @@ const getWatchHistory = asyncHandler(async (req, res) => {
 })
 
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile }
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory }
